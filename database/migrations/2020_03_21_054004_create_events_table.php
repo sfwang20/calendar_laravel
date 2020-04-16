58b31b744583306fd59021650758c585fa7c520b
Shin-Fu <shinfu619@gmail.com>
2020-04-16 10:17:02 +0800

    + migration  (create events table)

---
+++ database/migrations/2020_03_21_054004_create_events_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->integer('year');
+            $table->integer('month');
+            $table->integer('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function(Blueprint $table){
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('events');
+    }
+}
